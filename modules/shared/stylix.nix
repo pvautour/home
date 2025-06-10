8d487e0ddf64c8922914956a71e0bc57a70c0814
Pascal Vautour <vautour.pascal@gmail.com>
2025-06-10 16:53:16 -0400

    Found a sensible green screen theme and updates.

--- modules/shared/stylix.nix
+++ modules/shared/stylix.nix
@@ -17,23 +17,24 @@ with lib;
       polarity = "dark";
       image = ../../wallpapers/ferns-2.jpg;
       base16Scheme = {
-        base00 = "#001100"; # background
-        base01 = "#00ff00"; # lighter background
-        base02 = "#00bb00"; # selection background
-        base03 = "#007700"; # comments, invisibles
-        base04 = "#009900"; # dark foreground (status bars)
-        base05 = "#00bb00"; # main foreground text
-        base06 = "#005500"; # light foreground
-        base07 = "#00bb00"; # light background (unused mostly)
-        base08 = "#FFBF00"; # error (amber)  # Error - Amber
-        base09 = "#007700"; # accent (orange)
-        base0A = "#FFB642"; # warning (burnt yellow)  # Warning - Burnt Yellow
-        base0B = "#007700"; # success
-        base0C = "#009900"; # for regex, diff additions, etc.
-        base0D = "#00bb00"; # functions, keywords
-        base0E = "#005500"; # classes/types
-        base0F = "#00ff00"; # deprecated, custom
-        name = "CustomGreenAmberContrast";
+        name = "Green Screen";
+        author = "Chris Kempson (http://chriskempson.com)";
+        base00 = "001100";
+        base01 = "003300";
+        base02 = "005500";
+        base03 = "007700";
+        base04 = "009900";
+        base05 = "00bb00";
+        base06 = "00dd00";
+        base07 = "00ff00";
+        base08 = "007700";
+        base09 = "009900";
+        base0A = "007700";
+        base0B = "00bb00";
+        base0C = "005500";
+        base0D = "009900";
+        base0E = "00bb00";
+        base0F = "005500";
       };
     };
 
