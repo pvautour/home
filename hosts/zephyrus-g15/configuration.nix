e5ea7b87394983535734afa5f239c50ca98e550d
Pascal Vautour <vautour.pascal@gmail.com>
2025-06-13 08:23:11 -0400

    no longer auto enable bluetooth and laptop monitor should alway be to the left.

--- hosts/zephyrus-g15/configuration.nix
+++ hosts/zephyrus-g15/configuration.nix
@@ -43,9 +43,8 @@
 
   hardware.bluetooth = {
     enable = true; # enables support for Bluetooth
-    powerOnBoot = true; # powers up the default Bluetooth controller on boot
+    powerOnBoot = false; # powers up the default Bluetooth controller on boot
     package = pkgs.bluez;
-    settings.Policy.AutoEnable = "true";
   };
   services.blueman.enable = true;
 
